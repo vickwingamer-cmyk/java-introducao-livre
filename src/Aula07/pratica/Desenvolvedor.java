fa17a0b9c11e8ae68af59354ad8ce9f2dae85749
Vickz1 <vickwingamer@gmail.com>
2026-05-29 17:28:47 -0300

    Add constructor to Desenvolvedor class

--- src/Aula07/pratica/Desenvolvedor.java
+++ src/Aula07/pratica/Desenvolvedor.java
@@ -1,6 +1,8 @@
 package Aula07.pratica;
 
 public class Desenvolvedor extends Funcionario {
+    public Desenvolvedor(String nome, double salario) {
+        super(nome, salario);
 
     @Override
     double calcularBonus() {
