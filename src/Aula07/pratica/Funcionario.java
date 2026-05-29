0e0d7ab1ab8d750d123f3a89b1ef882576442845
Vickz1 <vickwingamer@gmail.com>
2026-05-29 17:25:36 -0300

    Update Funcionario.java

--- src/Aula07/pratica/Funcionario.java
+++ src/Aula07/pratica/Funcionario.java
@@ -3,9 +3,22 @@ package Aula07.pratica;
 public class Funcionario {
 	protected String nome;
 	protected double salario;
+
+	public Funcionario (String nome, double salario) {
+		this.nome = nome;
+		this.salario = salario;
+
+	}
 	
 	double calcularBonus() {
 		return salario * 0.10;
+	}
+	public String getNome() {
+		return nome;
+	}
+
+	public double getSalario() {
+		return salario;
 	}
 
 }
