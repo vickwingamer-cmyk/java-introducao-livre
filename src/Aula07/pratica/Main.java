c8c2e248408be6655d5221cd0977505395df8109
Vickz1 <vickwingamer@gmail.com>
2026-05-29 17:33:35 -0300

    Update Main.java

--- src/Aula07/pratica/Main.java
+++ src/Aula07/pratica/Main.java
@@ -6,30 +6,33 @@ public class Main {
 		ArrayList<Funcionario> funcionarios = new ArrayList<>();
 		
 		//INSTACIA CLASSE GERENTE 
-		Gerente g1 = new Gerente();
-		g1.nome = "Gabriel";
-		g1.salario = 10000;
+		//Gerente g1 = new Gerente();
+		 //g1.nome = "Gabriel";
+		//g1.salario = 10000;
 		
 		//INSTACIA CLASSE DESENVOLVEDOR 
 		
-		Desenvolvedor d1 = new Desenvolvedor();
-		d1.nome = "Bruno";
-		d1.salario = 7000;
+		//Desenvolvedor d1 = new Desenvolvedor();
+		//d1.nome = "Bruno";
+		//d1.salario = 7000;
 		
 		
 		// FUNCIONARIOS ADD
-		funcionarios.add(g1);
-        funcionarios.add(d1);
+		//funcionarios.add(g1);
+        //funcionarios.add(d1);
         
-        
-        //looping polimorfismo
+        funcionarios.add(new Gerente("Antonio", 4000.00));
+		funcionarios.add(new Desenvolvedor("Severino", 12000.00));
+		funcionarios.add(new Funcionario("Cleide", 6000.00));
+		
+        //looping 
         for (Funcionario f : funcionarios) {
 
-            System.out.println("Funcionário: " + f.nome);
+            System.out.println(f.getNome());
 			System.out.println("Salario total: " + f.salario());
             System.out.println("Bônus: " + f.calcularBonus());
-			System.out.println("Bônus: " + (f.salario + f.calcularBonus());
+			System.out.println("f.getSalario());
             System.out.println("----------------");
         }
     }
-}
+
