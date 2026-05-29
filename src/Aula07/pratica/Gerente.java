73edc0500672009a2c3ed8bc8ef0559d7b4587e3
Vickz1 <vickwingamer@gmail.com>
2026-05-29 17:27:53 -0300

    Update Gerente.java

--- src/Aula07/pratica/Gerente.java
+++ src/Aula07/pratica/Gerente.java
@@ -1,6 +1,8 @@
 package Aula07.pratica;
 
 	public class Gerente extends Funcionario {
+		public Gerente (String nome, double salario) {
+			super(nome, salario);
 	@Override
 	double calcularBonus() {
 		return salario * 0.20;
